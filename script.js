c26f072094506be6ac8d1336897d61db1d81e48a
Abdur Rehman Khalid <abdurehmank7@gmail.com>
2025-01-19 16:02:48 +0500

    formatted script.js

--- script.js
+++ script.js
@@ -1,24 +1,119 @@
 // IMPORTS
 import { CATEGORIES, initialFacts } from "./data.js";
 
-// console.log(initialFacts);
-
-// console.log("Hello World!")
-
+// DOM
 const btn = document.querySelector(".btn-open");
 // console.dir(btn); // gives btn as dom object with drop down
-
 const form = document.querySelector(".fact-form");
-
 const factsList = document.querySelector(".facts-list");
-// creating out embedded html list
-// factsList.innerHTML = "";
+const asideCategoryList = document.querySelector(".aside-cateogories-ul");
 
-// loading data from Supabase
+// clearing
+factsList.innerHTML = "";
 
-loadFacts();
+// UTILITY FUNCTIONS
+/**
+ *
+ * @param {Array} categoryObjList - List of Category Objects {name, color}
+ * @param {String} categoryName - Name of target category to find to color of
+ * @returns {String} - The corresponding color of category
+ */
+const findColor = (categoryObjList, categoryName) => {
+	// the answer was an object inside an array
+	const [filteredObject] = categoryObjList.filter((eachObject) => {
+		return eachObject.name === categoryName;
+	});
+	return filteredObject.color;
+};
 
-async function loadFacts(params) {
+/**
+ * Populates Categories of in the sidebar
+ */
+
+const populateCategories = () => {
+	// clear the existing categories except the first one
+	const lis = asideCategoryList.querySelectorAll("li:not(:first-child)");
+	lis.forEach((elem) => elem.remove());
+
+	// adding categories to side
+	CATEGORIES.forEach((elem) => {
+		const { name, color } = elem;
+		const categoryListItem = document.createElement("li");
+		categoryListItem.classList.add("category");
+
+		const categoryButton = document.createElement("button");
+		categoryButton.classList.add("btn", "btn-category");
+		categoryButton.style = `background-color: ${color}`;
+		categoryButton.textContent = `${name}`;
+
+		categoryListItem.appendChild(categoryButton);
+		asideCategoryList.appendChild(categoryListItem);
+	});
+};
+
+/**
+ * Creates a DOM element that is list item which represents each fact
+ * @param {Object} factObject - The factObject
+ * @returns {HTMLElement} - The list item
+ */
+
+const createFactListElement = (factObject) => {
+	const {
+		id,
+		text,
+		source,
+		category,
+		votesInteresting,
+		votesMindblowing,
+		votesFalse,
+		createdIn,
+	} = factObject;
+
+	// Parent list element
+	const listElement = document.createElement("li");
+	listElement.classList.add("fact");
+
+	// FACT: text and source
+	const paragraph = document.createElement("p");
+	paragraph.textContent = text;
+	listElement.appendChild(paragraph);
+
+	const anchorTag = document.createElement("a");
+	anchorTag.classList.add("source");
+	anchorTag.href = source;
+	anchorTag.target = "_blank";
+	anchorTag.textContent = "(Source)";
+	paragraph.append(anchorTag);
+
+	// Category tag
+	const spamElement = document.createElement("span");
+	spamElement.classList.add("tag");
+
+	const color = findColor(CATEGORIES, category);
+	spamElement.style = `background-color: ${color}`;
+	spamElement.textContent = category;
+	listElement.appendChild(spamElement);
+
+	// Vote buttons
+	const divButton = document.createElement("div");
+	divButton.classList.add("vote-buttons");
+
+	divButton.innerHTML = `
+		<button>👍 ${votesInteresting}</button>
+		<button>🤯 ${votesMindblowing}</button>
+		<button>⛔️ ${votesFalse}</button>
+
+	`;
+
+	listElement.appendChild(divButton);
+	return listElement;
+};
+
+/**
+ * - Loads facts from Supabase API and populates the facts list.
+ *
+ */
+async function loadFacts() {
 	const res = await fetch(
 		"https://gkcojvcxjiqhwrctfnxi.supabase.co/rest/v1/facts",
 		{
@@ -31,119 +126,19 @@ async function loadFacts(params) {
 	);
 
 	const data = await res.json();
-	// console.log(data);
 
 	data.forEach((fact) => {
-		const listElement = createListElement(fact);
+		const listElement = createFactListElement(fact);
 		factsList.insertAdjacentElement("beforeend", listElement);
 	});
 }
 
-const asideCategoryList = document.querySelector(".aside-cateogories-ul");
+// EVENT HANDLERS
 
-// remove the embedded category list items leaving all in place
-const lis = asideCategoryList.querySelectorAll("li:not(:first-child)");
-lis.forEach((elem) => elem.remove());
-// adding categories to side
-
-CATEGORIES.forEach((elem) => {
-	const { name, color } = elem;
-	const categoryListItem = document.createElement("li");
-	categoryListItem.classList.add("category");
-
-	const categoryButton = document.createElement("button");
-
-	categoryButton.classList.add("btn", "btn-category");
-	categoryButton.style = `background-color: ${color}`;
-	categoryButton.textContent = `${name}`;
-
-	categoryListItem.appendChild(categoryButton);
-
-	asideCategoryList.appendChild(categoryListItem);
-});
-
-// adding facts to the list dynamically from initialFacts along with the
-// input:categoryname, categoryObjList return color
-const findColor = (categoryObjList, categoryName) => {
-	// console.log(categoryObjList);
-
-	// the answer was an object inside an array
-	const [filteredObject] = categoryObjList.filter((eachObject) => {
-		return eachObject.name === categoryName;
-	});
-	return filteredObject.color;
-};
-
-// this function will return a list dom element to be added to unodered list
-// look at the initial html list elements for reference
-
-const createListElement = (factObject) => {
-	// destructure factObject
-	const {
-		id,
-		text,
-		source,
-		category,
-		votesInteresting,
-		votesMindblowing,
-		votesFalse,
-		createdIn,
-	} = factObject;
-
-	// create parent list element
-	const listElement = document.createElement("li");
-	// added class
-	listElement.classList.add("fact");
-
-	const paragraph = document.createElement("p");
-	// textContent is inner text that is displayed
-	paragraph.textContent = text;
-	// add paragraph element to parent element
-	listElement.appendChild(paragraph);
-
-	// create anchor DOM element for source, is clickable
-	const anchorTag = document.createElement("a");
-	anchorTag.classList.add("source");
-	anchorTag.href = source;
-	anchorTag.target = "_blank";
-	anchorTag.textContent = "(Source)";
-	paragraph.append(anchorTag);
-
-	// create DOM element for category
-	const spamElement = document.createElement("span");
-	spamElement.classList.add("tag");
-
-	const color = findColor(CATEGORIES, category);
-
-	spamElement.style = `background-color: ${color}`;
-	spamElement.textContent = category;
-	listElement.appendChild(spamElement);
-
-	// making div for buttons
-	const divButton = document.createElement("div");
-	divButton.classList.add("vote-buttons");
-	const likeButton = document.createElement("button");
-	likeButton.textContent = `👍 ${votesInteresting}`;
-	const minBlownButton = document.createElement("button");
-	minBlownButton.textContent = `🤯 ${votesMindblowing}`;
-	const dislikeButton = document.createElement("button");
-	dislikeButton.textContent = `⛔️ ${votesFalse}`;
-
-	divButton.appendChild(likeButton);
-	divButton.appendChild(minBlownButton);
-	divButton.appendChild(dislikeButton);
-
-	listElement.appendChild(divButton);
-	return listElement;
-};
-
-// initialFacts.forEach((elem) => {
-// 	const listElement = createListElement(elem);
-// 	factsList.insertAdjacentElement("beforeend", listElement);
-// });
-
-// Toggle form visibility
-btn.addEventListener("click", () => {
+/**
+ * Toggle visibility off the form
+ */
+const toggleFormVisibility = () => {
 	// console.log("Button was CLICKed");
 	if (form.classList.contains("hidden")) {
 		form.classList.remove("hidden");
@@ -152,4 +147,13 @@ btn.addEventListener("click", () => {
 		form.classList.add("hidden");
 		btn.textContent = "share a fact";
 	}
-});
+};
+
+const init = () => {
+	loadFacts();
+	populateCategories();
+
+	btn.addEventListener("click", toggleFormVisibility);
+};
+
+init();
